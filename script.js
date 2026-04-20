4cfbe1794c80c69c7eefbfd9cd5f5096bb2b1787
GC77XR <Devisionholding@gmail.com>
2026-04-20 16:12:42 -0400

    script.js

--- script.js
+++ script.js
@@ -21,5 +21,35 @@ startBtn.addEventListener('click', () => {
 
 function initiateCalibration() {
     console.log("NOS Vessel Ignited at 118 BPM...");
-    // The heartbeat oscillators will be added here next!
+    
+    // Schedule the heartbeat to start
+    setInterval(() => {
+        playHeartbeat();
+    }, beatInterval * 1000);
+}
+
+function playHeartbeat() {
+    const now = audioCtx.currentTime;
+
+    // THE LUB (Deep Thump)
+    const osc1 = audioCtx.createOscillator();
+    const gain1 = audioCtx.createGain();
+    osc1.frequency.setValueAtTime(60, now);
+    gain1.gain.setValueAtTime(0.5, now);
+    gain1.gain.exponentialRampToValueAtTime(0.001, now + 0.1);
+    osc1.connect(gain1);
+    gain1.connect(audioCtx.destination);
+    osc1.start(now);
+    osc1.stop(now + 0.1);
+
+    // THE DUB (Higher Snap)
+    const osc2 = audioCtx.createOscillator();
+    const gain2 = audioCtx.createGain();
+    osc2.frequency.setValueAtTime(90, now + 0.15);
+    gain2.gain.setValueAtTime(0.3, now + 0.15);
+    gain2.gain.exponentialRampToValueAtTime(0.001, now + 0.25);
+    osc2.connect(gain2);
+    gain2.connect(audioCtx.destination);
+    osc2.start(now + 0.15);
+    osc2.stop(now + 0.25);
 }
